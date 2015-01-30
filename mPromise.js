6183a0ab48e98f3d170358248835a592241e53c6
wengeezhang <wengeezhang@gmail.com>
2015-01-30 14:48:13 +0800

    阶段：延迟promise的多次调用后的分发处理

--- mPromise.js
+++ mPromise.js
@@ -1,120 +1,123 @@
     function mPromise(executor) {
         this.executed = false;
         this.state = "pending";
-        this.fullfilFun = null;
-        this.rejectFun = null;
+        this.fullfilFunArr = [];
+        this.rejectFunArr = [];
+        //fullfilResult是当前promise的运行结果，只有一个。
+        //即先有fullfilResult，然后再分发给fullfilFunArrArr
         this.fullfilResult = null;
         this.rejectResult = null;
-        this.subPromise = null;
+        this.subPromiseArr = [];
+        this.alreadyEquPro=null;
         this.returnedToVar=null;
-        this.protoThenCalled=false;
         this.executor = executor || null;
         if (this.executor == null) {
             return;
-        };
+        }
         var that = this;
         this.executor(function(e) {
             that.state = "fullfiled";
             that.fullfilResult = e;
             that.executed = true;
-            if(that.returnedToVar){//deferred promise（后面无then）的善尾工作
-              that.returnedToVar.state="fullfiled";
-              that.returnedToVar.fullfilResult = e;
-              that.returnedToVar.executed = true;
-              if(that.returnedToVar.subPromise){//check again whether returnedToVar calls then
-                //that.returnedToVar.then(that.returnedToVar.fullfilFun,that.returnedToVar.rejectFun);//不要二次调用then；其实进入到then后，还是执行thenExec
-                that.constructor.thenExec(that.returnedToVar,that.returnedToVar.fullfilFun,that.returnedToVar.fullfilResult);
+
+            if(that.alreadyEquPro){//只有then，且不是链条最后一个then中产生的promise，才可能有alreadyEquPro。
+              if(that.alreadyEquPro.subPromiseArr.length){
+                that.constructor.alreadyThenContinue(that.alreadyEquPro,that.alreadyEquPro.fullfilFunArr,e);
+              }else{
+                that.alreadyEquPro.fullfilResult=e;
+                that.alreadyEquPro.state="fullfiled";
+                that.alreadyEquPro.executed=true;
               }
-            }
-            if(that.subPromise){
-              that.constructor.thenExec(that,that.fullfilFun,that.fullfilResult);
+            }else{
+              if(that.subPromiseArr.length){
+                that.constructor.alreadyThenContinue(that,that.fullfilFunArr,e);
+              };
             }
         }, function(e) {
             that.state = "rejected";
             that.rejectResult = e;
             that.executed = true;
-            if(that.returnedToVar){//deferred promise（后面无then）的善尾工作
-              that.returnedToVar.state="rejected";
-              that.returnedToVar.rejectResult = e;
-              that.returnedToVar.executed = true;
-              if(that.returnedToVar.subPromise){//再次检测这中间等待时，外面的变量var有无then调用
-                that.constructor.thenExec(that.returnedToVar,that.returnedToVar.rejectFun,that.returnedToVar.rejectResult);
+            if(that.alreadyEquPro){//只有then，且不是链条最后一个then中产生的promise，才可能有alreadyEquPro。
+              if(that.alreadyEquPro.subPromiseArr.length){
+                that.constructor.alreadyThenContinue(that.alreadyEquPro,that.alreadyEquPro.rejectFunArr,e);
+              }else{
+                that.alreadyEquPro.rejectResult=e;
+                that.alreadyEquPro.state="rejected";
+                that.alreadyEquPro.executed=true;
               }
-            }
-            if(that.subPromise){
-              that.constructor.thenExec(that,that.rejectFun,that.rejectResult);
+            }else{
+              if(that.subPromiseArr.length){
+                that.constructor.alreadyThenContinue(that,that.rejectFunArr,e);
+              };
             }
         });
     };
-    mPromise.thenExec=function(that,thenArguFun,thatResult){
-      var result,cacheSubPromise=that.subPromise;
-      result=thenArguFun(thatResult);
-      if(result instanceof that.constructor){
-        //1.fullfilResult/rejectResult(自动)；2.fullfilFun/rejectFun;3.subPromise扶正
-        that.subPromise=result;
-        that.subPromise.fullfilFun=cacheSubPromise.fullfilFun;
-        that.subPromise.rejectFun=cacheSubPromise.rejectFun;
-        that.subPromise.subPromise=cacheSubPromise.subPromise;
-        if(!that.subPromise.subPromise){
-          that.subPromise.returnedToVar=cacheSubPromise;
-        }
-      }else{
-        that.subPromise=new that.constructor(function(fFlagFun){
-                fFlagFun(result);
-            });
-        that.subPromise.fullfilFun=cacheSubPromise.fullfilFun;
-        that.subPromise.rejectFun=cacheSubPromise.rejectFun;
-        that.subPromise.subPromise=cacheSubPromise.subPromise;
-        //既然进到else，that.subPromise就是同步的，需要检测链条中后续是否还有then调用，故状态一定是fullfiled
-        if(that.subPromise.subPromise){
-          //that.subPromise.then(cacheSubPromise.fullfilFun,cacheSubPromise.rejectFun);//不要二次调用then；其实进入到then后，还是执行thenExec
-          that.constructor.thenExec(that.subPromise,that.subPromise.fullfilFun,that.subPromise.fullfilResult);
-        }else{
-          cacheSubPromise.state=that.subPromise.state;
-          cacheSubPromise.fullfilResult=that.subPromise.fullfilResult;
-          cacheSubPromise.rejectResult=that.subPromise.rejectResult;
-          cacheSubPromise.executed=true;
-          that.subPromise.returnedToVar=cacheSubPromise;
-        }
+    mPromise.alreadyThenContinue=function(thenCalledPro,thenArguFunArr,thatResult){//之前的ThenExec
+      var result,thenGenePro;
+      for(var i=0;i<thenCalledPro.subPromiseArr.length;i++){
+          result=thenArguFunArr[i](thatResult);
+          if(result instanceof thenCalledPro.constructor){
+            //进到这里，that.subPromiseArr的执行肯定被延时啦
+            thenGenePro=result;
+            thenGenePro.alreadyEquPro=thenCalledPro.subPromiseArr[i];
+          }else{
+            thenGenePro=new thenCalledPro.constructor(function(fFlagFun){
+                    fFlagFun(result);
+                });
+            //上面产生的thenGenePro，在初始化中，没走ifelse中的任何逻辑。
+            thenCalledPro.subPromiseArr[i].fullfilResult=thenGenePro.fullfilResult;
+            thenCalledPro.subPromiseArr[i].state="fullfiled";
+            thenCalledPro.subPromiseArr[i].executed=true;
+            //既然进到else，that.subPromiseArr就是同步的，需要检测链条中后续是否还有then调用，故状态一定是fullfiled
+            if(thenCalledPro.subPromiseArr[i].subPromiseArr.length){
+              //that.subPromiseArr.then(cachesubPromiseArr.fullfilFunArr,cachesubPromiseArr.rejectFunArr);//不要二次调用then；其实进入到then后，还是执行thenExec
+              thenCalledPro.constructor.alreadyThenContinue(thenCalledPro.subPromiseArr[i],thenCalledPro.subPromiseArr[i].fullfilFunArr,result);
+            }
+          }
       }
     }
     mPromise.prototype.then = function(f, r) {
-        this.fullfilFun = f || null;
-        this.rejectFun = r || null;
-        this.protoThenCalled=true;
+        this.fullfilFunArr.push(f || null);
+        this.rejectFunArr.push(r || null);
         //1.设置链式子代-start
         //--promise has been defered  只需判断this.executed即可
+        var newestPromise;
         if(!this.executed){
-          this.subPromise=new mPromise();
-          return this.subPromise;
+          newestPromise=new mPromise();
+          this.subPromiseArr.push(newestPromise);
+          return newestPromise;
         }
         //设置链式子代-end
 
         //2.then代码块
         //执行then代码块--start
-        if(!this.subPromise || this.protoThenCalled){//一个promise只能调用一个then；如果二次调用，则表明它开启了一个新链条
-        //切记：过了1年，一个promise才再次调用then。（第二个链式开头的，如果this是同步的，属于此；如果是延时的，不属于此，它属于多个subPromise广播）
+        //if(!this.subPromiseArr.length || this.protoThenCalled){//一个promise只能调用一个then；如果二次调用，则表明它开启了一个新链条
+        //此if判断已不需要，因为综合发现，then只有一次调用，既然进到这里来，
+          //a.肯定是已经执行完了，而且this.subPromiseArr=null 或者
+          //b.同一个promise第二次调用；即a.then();a.then();
+        //切记：过了1年，一个promise才再次调用then。（第二个链式开头的，如果this是同步的，属于此；如果是延时的，不属于此，它属于多个subPromiseArr广播）
         //可以暂时忽略此片段，它只是用于过了n年才调用的情形；
           var result;
           if(this.state=="fullfiled"){
-            result=this.fullfilFun(this.fullfilResult);
+            result=this.fullfilFunArr.pop()(this.fullfilResult);
           }else{
-            result=this.rejectFun(this.rejectResult);
+            result=this.rejectFunArr.pop()(this.rejectResult);
           }
           if(result instanceof this.constructor){
-            this.subPromise=result;
+            newestPromise=result;
           }else{
-            this.subPromise=new this.constructor(function(fFlagFun){
+            newestPromise=new this.constructor(function(fFlagFun){
                     fFlagFun(result);
                 });
           }
-          return this.subPromise;//
-        }else{
+          this.subPromiseArr.push(newestPromise);
+          return newestPromise;//
+        /*}else{
           if(this.state=="fullfiled"){
-            this.constructor.thenExec(this,this.fullfilFun,this.fullfilResult);
+            this.constructor.thenExec(this,this.fullfilFunArr,this.fullfilResult);
           }else{
-            this.constructor.thenExec(this,this.rejectFun,this.rejectResult);
+            this.constructor.thenExec(this,this.rejectFunArr,this.rejectResult);
           }
-        }
+        }*/
         //执行then代码块--end
     }
