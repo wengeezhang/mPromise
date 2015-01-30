b9b5a9975b16590290b34154ad04a288131dfc26
wengeezhang <wengeezhang@gmail.com>
2015-01-30 17:14:07 +0800

    修复bug:异步promise还未执行，中间临时再次调用promise.then()，导致promise的执行完后，外围alreadyEquPro没有有同步执行结果

--- mPromise.js
+++ mPromise.js
@@ -1,3 +1,12 @@
+    /*
+    author:wengeezhang;
+    version:1.0;
+    
+    prototype:then
+    public static member:alreadyThenContinue
+    principle 1：every promise in a chain can't be replaced considering correct referring
+    principle 2：one promise only one fullfil/rejecResult,but can have an array of fullfil/rejecFun or subPromise
+    */
     function mPromise(executor) {
         this.executed = false;
         this.state = "pending";
@@ -9,7 +18,6 @@
         this.rejectResult = null;
         this.subPromiseArr = [];
         this.alreadyEquPro=null;
-        this.returnedToVar=null;
         this.executor = executor || null;
         if (this.executor == null) {
             return;
@@ -19,14 +27,13 @@
             that.state = "fullfiled";
             that.fullfilResult = e;
             that.executed = true;
-
             if(that.alreadyEquPro){//只有then，且不是链条最后一个then中产生的promise，才可能有alreadyEquPro。
+              that.alreadyEquPro.fullfilResult=e;
+              that.alreadyEquPro.executor=that.executor;
+              that.alreadyEquPro.state="fullfiled";
+              that.alreadyEquPro.executed=true;
               if(that.alreadyEquPro.subPromiseArr.length){
                 that.constructor.alreadyThenContinue(that.alreadyEquPro,that.alreadyEquPro.fullfilFunArr,e);
-              }else{
-                that.alreadyEquPro.fullfilResult=e;
-                that.alreadyEquPro.state="fullfiled";
-                that.alreadyEquPro.executed=true;
               }
             }else{
               if(that.subPromiseArr.length){
@@ -57,15 +64,17 @@
       for(var i=0;i<thenCalledPro.subPromiseArr.length;i++){
           result=thenArguFunArr[i](thatResult);
           if(result instanceof thenCalledPro.constructor){
-            //进到这里，that.subPromiseArr的执行肯定被延时啦
+            //进到这里，thenCalledPro.subPromiseArr[i]的执行肯定被延时啦
             thenGenePro=result;
             thenGenePro.alreadyEquPro=thenCalledPro.subPromiseArr[i];
+            //等到thenGenePro执行的时候，大致跟下面的else执行的任务一样。
           }else{
             thenGenePro=new thenCalledPro.constructor(function(fFlagFun){
                     fFlagFun(result);
                 });
-            //上面产生的thenGenePro，在初始化中，没走ifelse中的任何逻辑。
+            //上面产生的thenGenePro，在初始化中，没走executor中的if else的任何逻辑。
             thenCalledPro.subPromiseArr[i].fullfilResult=thenGenePro.fullfilResult;
+            thenCalledPro.subPromiseArr[i].executor=thenGenePro.executor;
             thenCalledPro.subPromiseArr[i].state="fullfiled";
             thenCalledPro.subPromiseArr[i].executed=true;
             //既然进到else，that.subPromiseArr就是同步的，需要检测链条中后续是否还有then调用，故状态一定是fullfiled
@@ -81,38 +90,37 @@
         this.rejectFunArr.push(r || null);
         //1.设置链式子代-start
         //--promise has been defered  只需判断this.executed即可
-        var newestPromise;
+        var result,thenGenePro;
         if(!this.executed){
-          newestPromise=new mPromise();
-          this.subPromiseArr.push(newestPromise);
-          return newestPromise;
+          thenGenePro=new mPromise();
+          this.subPromiseArr.push(thenGenePro);
+          return thenGenePro;
         }
         //设置链式子代-end
 
         //2.then代码块
         //执行then代码块--start
         //if(!this.subPromiseArr.length || this.protoThenCalled){//一个promise只能调用一个then；如果二次调用，则表明它开启了一个新链条
-        //此if判断已不需要，因为综合发现，then只有一次调用，既然进到这里来，
+        //此if判断已不需要，肯定会进到if里面。因为综合发现，then只有一次调用，既然进到这里来，
           //a.肯定是已经执行完了，而且this.subPromiseArr=null 或者
           //b.同一个promise第二次调用；即a.then();a.then();
         //切记：过了1年，一个promise才再次调用then。（第二个链式开头的，如果this是同步的，属于此；如果是延时的，不属于此，它属于多个subPromiseArr广播）
         //可以暂时忽略此片段，它只是用于过了n年才调用的情形；
-          var result;
           if(this.state=="fullfiled"){
             result=this.fullfilFunArr.pop()(this.fullfilResult);
           }else{
             result=this.rejectFunArr.pop()(this.rejectResult);
           }
           if(result instanceof this.constructor){
-            newestPromise=result;
+            thenGenePro=result;
           }else{
-            newestPromise=new this.constructor(function(fFlagFun){
+            thenGenePro=new this.constructor(function(fFlagFun){
                     fFlagFun(result);
                 });
           }
-          this.subPromiseArr.push(newestPromise);
-          return newestPromise;//
-        /*}else{
+          this.subPromiseArr.push(thenGenePro);
+          return thenGenePro;//
+          /*}else{
           if(this.state=="fullfiled"){
             this.constructor.thenExec(this,this.fullfilFunArr,this.fullfilResult);
           }else{
