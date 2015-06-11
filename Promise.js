3d13bf6270f5388e8609206e3488f1d5f30a69dc
wengeezhang <wengeezhang@gmail.com>
2015-06-11 15:36:49 +0800

    annotation update-And any <then> being the end of a chain which is returned inside another <then> must be checked because of bug1

--- Promise.js
+++ Promise.js
@@ -1,28 +1,38 @@
     /*
     author:wengeezhang;
-    version:1.0;
+    version:1.5;
+    updated 2015/6/11
 
     ************
     prototype:     then,catch
     static method: all,race,resolve,reject
 
     ************
-    Example Analysis:
+    ** EXAMPLE
     x_promise.then(F1,F2).then(F3,F4).[---].then(Fm,Fm).
-    
+    ** ANALYSIS
     x_promise:      a promise which is async.
     chain:          promise(x_promise)->promise(F1/2)->promise(F3/4)...->promise(Fm/n)
     promise_Holder: an empty promise returned immediately by x_promise.then(F1,F2).In chain.
     promise_air:    a promise returned by F1/2();Not in chain.
-    placeholder:    a property of promise_air.It refers to promise_Holder.
-    
+    placeholder:    a property of promise_air;    It refers to promise_Holder.
     promise_air.placeholder=promise_Holder.
 
+    ************
     promise_Holder is in the chain and holds original information(subPromise) of the chain.
     promise_air isn't in chain,but it can self be executed in the future and will catch the result and state.
     finally,refresh promise_Holder's info with promise_air's. (bridge is placeholder)
     *:then's inner's return is a promise_air,and it's placeholder is then's promise_Holder.
     
+    *IMPORTANT:
+    Every <then> has its only one return,and this return creats/is the promise_air;
+    When return a chain,returning the last <then>'s promise_Holder; 
+
+    So,when we check a <then>'s promise_air,this promise_air's *promise_Holder* 
+         *may has subPromises,
+         *may be the end of a chain and be returned,so it becomes another promise_air
+    And any <then> being the end of a chain which is returned inside another <then> must be checked because of bug1
+
     ************
     principle 1： every promise in a chain can't be replaced considering correct referring
     principle 2： one promise can have only one fullfilResult/rejectResult,but can have an array of fullfilFuns/rejecFuns or subPromises
@@ -31,6 +41,10 @@
     ************
     bug1:
         then()'s return--promise chain;
+        var a=x_promise.then(function(value){
+              return y_promise.then(function(){/ajax process/});
+        })
+        a.then()
     */
     function Promise(executor) {
         this.executed = false;
@@ -50,16 +64,18 @@
             that.state = "resolved";
             that.fullfilResult = e;
             that.executed = true;
-            if(that.placeholder){//只有then，且不是链条最后一个then中产生的promise，才可能有placeholder。
+            if(that.placeholder){
               that_placeholder=that.placeholder;
               that_placeholder.fullfilResult=e;
               that_placeholder.state="resolved";
               that_placeholder.executed=true;
-              if(that_placeholder.subPromiseArr.length){
+              if(that_placeholder.subPromiseArr.length){//looks forward 
                 that.constructor.execThenOf(that_placeholder);
               }
-              //以下解决bug1:
+              //below fixes bug1:
               if(that_placeholder.placeholder){
+                //that_placeholder was end of chain,and the chain is returned inside a <then>
+                //so that_placeholder becomes a promise_air
                 that_placeholder.placeholder.fullfilResult=e;
                 that_placeholder.placeholder.state="resolved";
                 that_placeholder.placeholder.executed=true;
@@ -76,7 +92,7 @@
             that.state = "rejected";
             that.rejectResult = e;
             that.executed = true;
-            if(that.placeholder){//只有then，且不是链条最后一个then中产生的promise，才可能有placeholder。
+            if(that.placeholder){//
               that_placeholder=that.placeholder;
               that_placeholder.rejectResult=e;
               that_placeholder.state="rejected";
@@ -99,9 +115,10 @@
             }
         });
     };
-    Promise.execThenOf=function(thenCalledPro){//之前的ThenExec
-      //thenCalledPro:promise that called "then"
-      var result,thenGenePro,thenArguFunArr,supResult,promise_Holder,promise_air;
+    Promise.execThenOf=function(thenCalledPro){//preceding ThenExec
+      //thenCalledPro:promise calling "then"
+      var result,thenGenePro,thenArguFunArr,supResult;
+      var promise_Holder,promise_air;
       var subPromiseLen,curSubPromise;
       if(thenCalledPro.state=="resolved"){
         thenArguFunArr=thenCalledPro.fullfilFunArr;
@@ -118,8 +135,9 @@
             curSubPromise.executed=true;
             if(thenCalledPro.state=="resolved"){
               curSubPromise.fullfilResult=thenCalledPro.fullfilResult;
-              //以下解决bug1：
+              //below fixes bug1：
               if(curSubPromise.placeholder){
+                //curSubPromise may be then end of a chain which is returned inside a <then>
                 curSubPromise.placeholder.fullfilResult=curSubPromise.fullfilResult;
                 curSubPromise.placeholder.state="resolved";
                 curSubPromise.placeholder.executed=true;
@@ -129,7 +147,7 @@
               }
             }else{
               curSubPromise.rejectResult=thenCalledPro.rejectResult;
-              //以下解决bug1:
+              //bolow fixes bug1:
               if(curSubPromise.placeholder){
                 curSubPromise.placeholder.rejectResult=curSubPromise.rejectResult;
                 curSubPromise.placeholder.state="rejected";
@@ -145,7 +163,7 @@
               promise_air=result;
               promise_Holder=curSubPromise;
               promise_air.placeholder=promise_Holder;
-              if(promise_air.executed){//then的参数函数返回如下：new Promise(function(res){res("ss")})
+              if(promise_air.executed){//like this:new Promise(function(res){res("ss")})
                 if(promise_air.state=="resolved"){
                   promise_Holder.fullfilResult=promise_air.fullfilResult;
                   promise_Holder.state="resolved";
@@ -155,15 +173,13 @@
                 }
                 promise_Holder.executed=true;
                 
-              }//else情形下，将会在未来执行（构造函数内部的代码）setTimeout(,0) belongs to this
-              //等到thenGenePro执行的时候，大致跟下面的else执行的任务一样。
-              //bug:如果result是同步的（new Promise(function(res){res("ss")})），那么在设置thenGenePro.placeholder以前，构造函数内部的executor已经执行完了。
+              }
             }else{
-              //then的参数函数返回普通数据，如return "ok";所以state一定是resolved,没有rejected
+              //must be resolved
               curSubPromise.fullfilResult=result;
               curSubPromise.state="resolved";
               curSubPromise.executed=true;
-              //以下解决bug1:
+              //below fixes bug1:
               if(curSubPromise.placeholder){
                 curSubPromise.placeholder.fullfilResult=result;
                 curSubPromise.placeholder.state="resolved";
@@ -172,7 +188,6 @@
                   curSubPromise.constructor.execThenOf(curSubPromise.placeholder);
                 }
               }
-              //既然进到else，that.subPromiseArr[i]就是同步的，需要检测链条中后续是否还有then调用，故状态一定是resolved
             }
           }
       }
@@ -188,68 +203,52 @@
     Promise.prototype.then = function(f, r) {
         this.fullfilFunArr.push(f || null);
         this.rejectFunArr.push(r || null);
-        //1.设置链式子代-start
-        //--promise has been defered  只需判断this.executed即可
+        
         var result,thenGenePro,upperArgFun;
+        //defered --start
         if(!this.executed){
           thenGenePro=new this.constructor();
-          this.subPromiseArr.push(thenGenePro);//async,we call this empty promse "promise_Holder".
+          this.subPromiseArr.push(thenGenePro);//async,we call this empty promise "promise_Holder".
           return thenGenePro;
         }
-        //设置链式子代-end
+        //defered --end
 
-        //2.then代码块
-        //执行then代码块--start
-        //if(!this.subPromiseArr.length || this.protoThenCalled){//一个promise只能调用一个then；如果二次调用，则表明它开启了一个新链条
-        //此if判断已不需要，肯定会进到if里面。因为综合发现，then只有一次调用，既然进到这里来，
-          //a.肯定是已经执行完了，而且this.subPromiseArr=null 或者
-          //b.同一个promise第二次调用；即a.then();a.then();
-        //切记：过了1年，一个promise才再次调用then。（第二个链式开头的，如果this是同步的，属于此；如果是延时的，不属于此，它属于多个subPromiseArr广播）
-        //可以暂时忽略此片段，它只是用于过了n年才调用的情形；
-          if(this.state=="resolved"){
-            if(this.fullfilResult && typeof this.fullfilResult.then == "function"){//resolve(thenable)
-              this.fullfilResult.then(f);
-              return new this.constructor();
-            }
-            upperArgFun=this.fullfilFunArr.pop();
-            if(upperArgFun==null){
-              thenGenePro=new this.constructor();
-              thenGenePro.state="resolved";
-              thenGenePro.executed=true;
-              thenGenePro.fullfilResult=this.fullfilResult;
-              return thenGenePro;
-            }else{
-              result=upperArgFun(this.fullfilResult);
-            }
-          }else{
-            upperArgFun=this.rejectFunArr.pop();
-            if(upperArgFun==null){
-              thenGenePro=new this.constructor();
-              thenGenePro.executed=true;
-              thenGenePro.state="rejected";
-              thenGenePro.rejectResult=this.rejectResult;
-              return thenGenePro;
-            }else{
-              result=upperArgFun(this.rejectResult);
-            }
+        if(this.state=="resolved"){
+          if(this.fullfilResult && typeof this.fullfilResult.then == "function"){//resolve(thenable)
+            this.fullfilResult.then(f);
+            return new this.constructor();
           }
-          if(result instanceof this.constructor){
-            thenGenePro=result;
+          upperArgFun=this.fullfilFunArr.pop();
+          if(upperArgFun==null){
+            thenGenePro=new this.constructor();
+            thenGenePro.state="resolved";
+            thenGenePro.executed=true;
+            thenGenePro.fullfilResult=this.fullfilResult;
+            return thenGenePro;
           }else{
-            thenGenePro=new this.constructor(function(fFlagFun){
-                    fFlagFun(result);
-                });
+            result=upperArgFun(this.fullfilResult);
           }
-          this.subPromiseArr.push(thenGenePro);
-          return thenGenePro;//
-          /*}else{
-          if(this.state=="resolved"){
-            this.constructor.thenExec(this,this.fullfilFunArr,this.fullfilResult);
+        }else{
+          upperArgFun=this.rejectFunArr.pop();
+          if(upperArgFun==null){
+            thenGenePro=new this.constructor();
+            thenGenePro.executed=true;
+            thenGenePro.state="rejected";
+            thenGenePro.rejectResult=this.rejectResult;
+            return thenGenePro;
           }else{
-            this.constructor.thenExec(this,this.rejectFunArr,this.rejectResult);
+            result=upperArgFun(this.rejectResult);
           }
-        }*/
-        //执行then代码块--end
+        }
+        if(result instanceof this.constructor){
+          thenGenePro=result;
+        }else{
+          thenGenePro=new this.constructor(function(fFlagFun){
+                  fFlagFun(result);
+              });
+        }
+        this.subPromiseArr.push(thenGenePro);
+        return thenGenePro;//
     }
     Promise.prototype.catch=function(callback){
       return this.then(null,callback);
